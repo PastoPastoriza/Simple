47ee8618bf879f790f2a9ad9a1664aecab7b5dfc
PastoPastoriza <162369027+PastoPastoriza@users.noreply.github.com>
2024-06-21 13:50:31 -0300

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,11 @@
+def flatten(df, window):
+  """
+  Le das un df y te crea uno flatten con los rows con su lag de WINDOW
+  """
+  dfs = [df]
+
+  for i in range(1, window + 1):
+      lagged_df = df.shift(i).add_suffix(f'_{i}')
+      dfs.append(lagged_df)
+  result_df = pd.concat(dfs, axis=1)
+  return result_df
