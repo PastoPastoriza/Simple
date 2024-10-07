24f923cf8d4cc8f872a00e2b72beea29fe9d3019
PastoPastoriza <162369027+PastoPastoriza@users.noreply.github.com>
2024-10-07 15:21:30 -0300

    Update simple.py

--- simple.py
+++ simple.py
@@ -35,3 +35,56 @@ def create_binary_features(data, features):
         binary_df[f'{feat1}_gt_{feat2}'] = (data[feat1] > data[feat2]).astype(int)
 
     return binary_df
+#############
+# from sklearn.svm import SVC,NuSVC,LinearSVC
+# from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, ExtraTreesClassifier, AdaBoostClassifier,StackingClassifier,BaggingClassifier,HistGradientBoostingClassifier
+# from sklearn.ensemble import VotingClassifier
+# from sklearn.linear_model import LogisticRegression,LogisticRegressionCV,PassiveAggressiveClassifier,SGDClassifier,ElasticNet,MultiTaskElasticNet,Ridge
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.naive_bayes import GaussianNB,BernoulliNB
+# from sklearn.neural_network import MLPClassifier, BernoulliRBM
+# from sklearn.random_projection import GaussianRandomProjection
+# from sklearn.tree import DecisionTreeClassifier,ExtraTreeClassifier
+  
+  
+# models = [
+#     ("SVC (balanced)", SVC(class_weight="balanced")),
+#     ("SVC", SVC()),
+#     # ("NuSVC", NuSVC()),
+#     ("Logistic Regression (balanced)", LogisticRegression(class_weight="balanced")),
+#     ("Logistic Regression", LogisticRegression()),
+#     ("Random Forest (balanced)", RandomForestClassifier(class_weight="balanced")),
+#     ("Random Forest", RandomForestClassifier()),
+#     ("Gradient Boosting", GradientBoostingClassifier()),
+#     ("Extra Trees", ExtraTreesClassifier()),
+#     ("AdaBoost", AdaBoostClassifier())
+# ]
+
+# all_model_results = pd.DataFrame()
+
+# for name, model in models:
+#     np.random.seed(42)
+#     model.fit(X_train_array, y_train_array)
+#     model_pred = model.predict(X_test_array)
+
+#     # Assuming fun.true_dict returns a dictionary with evaluation metrics
+#     print(name)
+#     model_results = fun.true_dict(y_test_array, model_pred)
+#     all_model_results[name] = model_results
+
+# all_model_results = all_model_results.transpose()
+
+# # Plot all results
+# plt.style.use("seaborn-v0_8-whitegrid")
+# all_model_results.plot(kind="bar", figsize=(10, 7)).legend(bbox_to_anchor=(1.0, 1.0))
+# plt.title('Model Performance Comparison')
+# plt.show()
+
+# # Filter and plot based on precision_1
+# filtered_df = all_model_results[all_model_results['precision_1'] >= 0.4]
+# filtered_df.plot(kind="bar", figsize=(10, 7)).legend(bbox_to_anchor=(1.0, 1.0))
+# plt.title('Filtered Model Performance (Precision >= 0.4)')
+# plt.show()
+
+# filtered_df
+#######################
